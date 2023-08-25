7f545e318d6997a397bc65114f3f5e69c727aa8a
25JuanP <141178364+25JuanP@users.noreply.github.com>
2023-08-25 11:25:06 -0500

    juan

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 // REFERENCIA EL MODULO FUNCIONES A TRAVES DE LA CONSTANTE FUNCIONES
-const funciones = require('./modules/math.js')
 
-//console.log(funciones);
-Process.stdout.write("Ejercicio\n\n");
-console.log(funciones.add(5,3));
-console.log(funciones.sustract(2,9));
-console.log(funciones.multiply(8,8));
+process.stdout.write("Ejercicio\n\n");
 
-
-let nombreAprendiz = 'nicolas';
-let apellidoAprendiz= 'beLTRaN rivera'
+let nombreAprendiz = 'juan manuel';
+let apellidoAprendiz= 'rodriguez '
 
 let nombreCompletoAprendiz = nombreAprendiz[0].toUpperCase()+
 nombreAprendiz.substring(1).toLowerCase() + " " +
@@ -17,10 +11,12 @@ apellidoAprendiz.substring(0).toUpperCase() + apellidoAprendiz.substring(1).toLo
 
 console.log(nombreCompletoAprendiz);
 
-let i =nombreCompletoAprendiz.length;
+let i =nombreCompletoAprendiz.length
+let y =1;
 while(i > 0){
-    console.log(nombreCompletoAprendiz.length - nombreCompletoAprendiz.substring(0,i)).length +1 + '  '
+    console.log(i +"  "+ nombreCompletoAprendiz.substring(0,i));
     i--;
+    y++;
 }
  /*for (let index = 0; index < array.length; index++){
     const element = array[index];
